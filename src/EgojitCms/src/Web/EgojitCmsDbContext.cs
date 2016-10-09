33c85851f58b6c8d441a2f3a88943b5162a43617
egojit <408365330@qq.com>
2016-10-09 20:27:34 +0800

    dotnet ef migragtions add

---
+++ src/EgojitCms/src/Web/EgojitCmsDbContext.cs
@@ -0,0 +1,31 @@
+﻿using EgojitCms.web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgojitCms
+{
+    public class EgojitCmsDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySql(@"Server=localhost;database=cms;uid=root;pwd=egojit;");
+            //optionsBuilder.UseSqlite("Filename=./egojitcms.db");
+        }
+
+
+        /// <summary>
+        /// 用户
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// 账户
+        /// </summary>
+        public DbSet<Account> Accounts { get; set; }
+
+    }
+}
